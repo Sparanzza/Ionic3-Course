df1120695806d82a234c34707199aea37cb457ea
Sparanzza <aurelio@sparanzza.com>
2019-07-08 23:28:56 +0200

    Add prompt for user login  and loading controller

--- EX04_TaxiTracker/src/pages/login/login.ts
+++ EX04_TaxiTracker/src/pages/login/login.ts
@@ -1,5 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import {  IonicPage,
+          NavController,
+          NavParams,
+          Slides,
+          AlertController,
+          LoadingController
+        } from 'ionic-angular';
+
 
 /**
  * Generated class for the LoginPage page.
@@ -15,12 +22,58 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 })
 export class LoginPage {
   @ViewChild(Slides) slides: Slides;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(
+    private alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public loading: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
     this.slides.paginationType = 'progress';
+    this.slides.freeMode = false;
+
+  }
+
+  presentPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            console.log(data);
+            this.verifyUser(data.username);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  verifyUser(clave: String){
+    let loading = this.loading.create({
+      content: 'Please wait...'
+    });
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 2000);
 
   }
 }
