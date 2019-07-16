e476b39fa4f1caa6fb8a6e2016b3b606970ef3d0
Sparanzza <aurelio@sparanzza.com>
2019-07-16 22:50:54 +0200

    Save current position in firestore

--- EX04_TaxiTracker/src/providers/location/location.ts
+++ EX04_TaxiTracker/src/providers/location/location.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { UserProvider } from '../user/user';
 
 @Injectable()
 export class LocationProvider {
 
+
+  taxiDriver: AngularFirestoreDocument<any>;
   constructor(
+    public db: AngularFirestore,
+    public _userProvider: UserProvider,
     private geolocation: Geolocation
   ) {
     console.log('Hello LocationProvider Provider');
+    this.taxiDriver = db.doc(`/taxiUsers/${_userProvider.key}`);
+
   }
 
   initGeo(){
@@ -16,13 +24,25 @@ export class LocationProvider {
     // resp.coords.latitude
     // resp.coords.longitude
     console.log(resp.coords);
+    this.taxiDriver.update({
+      lat: resp.coords.latitude,
+      lng: resp.coords.longitude,
+      clave: this._userProvider.key
+      
+    });
 
     let watch = this.geolocation.watchPosition();
     watch.subscribe((data) => {
-    // data can be a set of coordinates, or an error (if an error occurred).
-    // data.coords.latitude
-    // data.coords.longitude
-    console.log("watch: " , data.coords);
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+      console.log("watch: " , data.coords);
+      this.taxiDriver.update({
+        lat: data.coords.latitude,
+        lng: data.coords.longitude,
+        clave: this._userProvider.key
+        
+      });
     });
 
    }).catch((error) => {
